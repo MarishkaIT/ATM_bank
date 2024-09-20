ca03b527cb73ce08f5040039fa178ca9881b581c
Marina <112541429+MarishkaIT@users.noreply.github.com>
2024-09-20 16:48:30 +0200

    first commit

---
+++ src/main/java/com/atm/bank/atm_bank/AtmBankApplication.java
@@ -0,0 +1,13 @@
+package com.atm.bank.atm_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atm/bank/atm_bank/AtmBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atm.bank.atm_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
